42c58ce34f9693a467134bfd98ddbc95174db7ab
Victor Hugo <victor-f250@hotmail.com>
2022-09-05 11:21:08 -0300

    front

--- front-end/tutorial-todo/src/App.js
+++ front-end/tutorial-todo/src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const arrayTodos = [
+  { name:"limparCasa", status:false}, 
+  { name:"Jhon", status:false }
+];
+
+const Todos = ({ todos }) => {
+  return <div className="todos">
+      {todos.map(todo => {
+        return (
+          <div className="todo">
+            <p>{todo.name}</p>
+          </div>
+        )
+      })}
+    </div>
+  };
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="container">
+        <Todos todos={arrayTodos}></Todos>
       </header>
     </div>
   );
