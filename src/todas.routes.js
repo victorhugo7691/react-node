e83b0b9e7e9693ee7a593f6c42e1312663b6201e
Victor Hugo <victor-f250@hotmail.com>
2022-09-05 10:50:29 -0300

    up

--- src/todas.routes.js
+++ src/todas.routes.js
@@ -9,8 +9,36 @@ todasRoutes.post("/todos", (request, response) => {
     return response.status(201).json(allTodos);
 });
 
-todasRoutes.get("/todos", (request, response)=> {
+todasRoutes.get("/todos", (request, response) => {
     return response.status(200).json(allTodos);
+});
+
+todasRoutes.put("/todos", (request, response) => {
+    const { name, status } = request.body;
+    const allTodos = allTodos.update({
+        where: {
+            name,
+        },
+        data: {
+            name,
+            status,
+        },
+    });
+});
+
+todasRoutes.delete("/todos/:name", (request, response) => {
+    const { name } = request.params;
+    if(!name){
+        return response.status(400).json("Nome e obrigatorio");
+    }
+    existeTodos = allTodos({ where: { name }});
+
+    if(!existeTodos){
+        return response.status(404).json("Nao existe");
+    }
+
+    allTodos.delete({ where: { name }});
+    return response.status(200).send;
 });
 
 module.exports = todasRoutes;
