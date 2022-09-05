3852a925e9877dc6fc3fd29b185ba3c81d3b8968
Victor Hugo <victor-f250@hotmail.com>
2022-09-05 10:09:21 -0300

    up

--- src/todas.routes.js
+++ src/todas.routes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 
-const allTodos = [];
+const allTodos = [{ name: "aaaaa", status: false }];
 const todasRoutes = express.Router();
 
 todasRoutes.post("/todos", (request, response) => {
-    const {name} = request.body;
-    allTodos.push({name, status: false});
+    const { name } = request.body;
+    allTodos.push({ name, status: false });
     return response.status(201).json(allTodos);
+});
+
+todasRoutes.get("/todos", (request, response)=> {
+    return response.status(200).json(allTodos);
 });
 
 module.exports = todasRoutes;
